487c3220e4978a79ac4bf8b955ab1c1e1b4ba853
ohy0718 <ohy0718@gmail.com>
2024-06-09 03:17:43 +0900

    1: 인덱스 페이지 추가 2: 인덱스 컨트롤러 추가 3: postgresql config 클래스 추가

--- src/main/java/ohy/web/WebApplication.java
+++ src/main/java/ohy/web/WebApplication.java
@@ -1,13 +1,14 @@
 package ohy.web;
 
+import org.glassfish.jaxb.core.annotation.OverrideAnnotationOf;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-
 }
