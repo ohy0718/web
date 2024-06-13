605ec0700acf60341a100a658baff31354df897b
ohy0718 <ohy0718@gmail.com>
2024-06-14 01:26:41 +0900

    gitignore 수정

--- src/main/java/ohy/web/controller/MainController.java
+++ src/main/java/ohy/web/controller/MainController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class FirstController {
+public class MainController {
   @RequestMapping(value="/", method=RequestMethod.GET)
   public String initMainPage() {
-      return "index.html";
+    return "index.html";
   }  
 }
